7006cf6b86f6e6feaf38759e4d14d3372a3491bb
ellisrugazoora <ellisrgrz@gmail.com>
2024-02-01 01:33:58 -0800

    Require login
    I wrapped Require login around the root of the App.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ import StockApp from "./Components/StockApp";
 import Music from './Components/Music/Music';
 
 import { Box, ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react';
+import { ThinBackend } from 'thin-backend-react';
 
 function App() {
   var display = {a: <StockApp />, b: <Tanga />, c: <Music />}
   return (
+  <ThinBackend requireLogin>
     <ChakraProvider>
-      
       <Box className="App" width={"100%"}>
         {display.b}
       </Box>
-      
     </ChakraProvider>
+  </ThinBackend>
       
-    
-    
   );
 }
 
