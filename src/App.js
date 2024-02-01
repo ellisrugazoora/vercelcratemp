44c11d4c02c4720d204d0b162ceb67d93158377f
ellisrugazoora <ellisrgrz@gmail.com>
2024-02-01 01:42:23 -0800

    added initthinBackend

--- src/App.js
+++ src/App.js
@@ -7,9 +7,15 @@ import Music from './Components/Music/Music';
 
 import { Box, ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react';
 import { ThinBackend } from 'thin-backend-react';
+import { ensureIsUser, initThinBackend } from 'thin-backend';
 
 function App() {
   var display = {a: <StockApp />, b: <Tanga />, c: <Music />}
+  initThinBackend({
+    // This url is different for each backend, this one points to 'vercelcratemp'
+    host: 'https://vercelcratemp.thinbackend.app'
+  });
+  
   return (
   <ThinBackend requireLogin>
     <ChakraProvider>
