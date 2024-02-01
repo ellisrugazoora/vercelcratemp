7fef3834c775b766b44b2a4b2fc6ebce06fd772d
ellisrugazoora <ellisrgrz@gmail.com>
2024-02-01 01:59:09 -0800

    troubleshooting
    commented out logout related items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 //import './App.css';
-
+import { Box, ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react';
+import { ThinBackend } from 'thin-backend-react';
+import { ensureIsUser, initThinBackend, logout } from 'thin-backend';
 import Tanga from './Components/Tanga/Tanga';
 import StockApp from "./Components/StockApp";
 import Music from './Components/Music/Music';
 
-import { Box, ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react';
-import { ThinBackend } from 'thin-backend-react';
-import { ensureIsUser, initThinBackend, logout } from 'thin-backend';
+
 
 function App() {
   var display = {a: <StockApp />, b: <Tanga />, c: <Music />}
@@ -15,17 +15,17 @@ function App() {
     // This url is different for each backend, this one points to 'vercelcratemp'
     host: 'https://vercelcratemp.thinbackend.app'
   });
-  function UserStatus() {
-    // Use the `useCurrentUser()` react hook to access the current logged in user
-    // Returns `null` while the user is being fetched
-    const user = useCurrentUser();
+  // function UserStatus() {
+  //   // Use the `useCurrentUser()` react hook to access the current logged in user
+  //   // Returns `null` while the user is being fetched
+  //   const user = useCurrentUser();
 
-    return <div>
-        {user?.email}
+  //   return <div>
+  //       {user?.email}
 
-        <button onClick={logout}>Logout</button>
-    </div>
-  }
+  //       <button onClick={logout}>Logout</button>
+  //   </div>
+  // }
   return (
   <ThinBackend requireLogin>
     <ChakraProvider>
